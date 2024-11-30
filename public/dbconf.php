22f7739189d7249dfb326f869aeac2dec35946e4
Ishara Basnayaka <139110442+isharabasnayaka@users.noreply.github.com>
2024-11-30 18:27:41 +0000

    add security

--- public/dbconf.php
+++ public/dbconf.php
@@ -1,9 +1,10 @@
 <?php
 //constant variable
-define('SERVERNAME', '127.0.0.1:3306');
-define('USERNAME', 'root');
-define('PASSWORD', 'mariadb');
-define('DBNAME', 'school');
+$dbHost = getenv('DB_HOST');
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
+
 try {
 	//connect with database
 	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
